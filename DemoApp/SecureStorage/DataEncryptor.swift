f3694537f7cb3fa610d7e525b3ddc24bcf3018e4
Adarsh Rai <adrai75@gmail.com>
2018-01-26 00:23:24 +0530

    Refactored to use bytes extension on Data class.

--- DemoApp/SecureStorage/DataEncryptor.swift
+++ DemoApp/SecureStorage/DataEncryptor.swift
@@ -11,69 +11,56 @@ import Foundation
 
 extension Data {
     
-    func encryptWithAES256(using key: Data, iv: Data) throws -> Data? {
+    func encryptWithAES256(using key: Data, iv: Data) throws -> Data {
         
         var encryptedData = Data(count: size_t(kCCBlockSizeAES128 + self.count + kCCBlockSizeAES128))
         var numBytesEncrypted :size_t = 0
         
-        let cryptStatus = encryptedData.withUnsafeMutableBytes { cryptBytes in
-            self.withUnsafeBytes { dataBytes in
-                key.withUnsafeBytes { (keyBytes: UnsafePointer<UInt8>) in
-                    iv.withUnsafeBytes { (ivBytes: UnsafePointer<UInt8>) in
-                        CCCrypt(CCOperation(kCCEncrypt),
-                                CCAlgorithm(kCCAlgorithmAES),
-                                CCOptions(kCCOptionPKCS7Padding),
-                                keyBytes,
-                                kCCKeySizeAES256,
-                                ivBytes,
-                                dataBytes,
-                                self.count,
-                                cryptBytes,
-                                self.count + kCCBlockSizeAES128,
-                                &numBytesEncrypted)
-                    }
-                }
-            }
-        }
-        if UInt32(cryptStatus) == UInt32(kCCSuccess) && numBytesEncrypted > 0 {
+        let status = CCCrypt(CCOperation(kCCEncrypt),
+                             CCAlgorithm(kCCAlgorithmAES),
+                             CCOptions(kCCOptionPKCS7Padding),
+                             key.pointer,
+                             kCCKeySizeAES256,
+                             iv.pointer,
+                             self.pointer,
+                             self.count,
+                             encryptedData.mutablePointer(),
+                             self.count + kCCBlockSizeAES128,
+                             &numBytesEncrypted)
+        
+        if UInt32(status) == UInt32(kCCSuccess) && numBytesEncrypted > 0 {
             var completeEncryptedData = Data()
             completeEncryptedData.append(encryptedData)
             completeEncryptedData.append(iv)
             return completeEncryptedData
         } else {
-            //Throw Error here
-            return nil
+            throw SecureStorageError.encryptionFailed
         }
     }
     
     
     func decryptWithAES256(using key: Data) throws -> Data {
+        
         let iv = self.subdata(in: self.count - kCCBlockSizeAES128 ..< self.count)
         let dataToBeDecrypted = self.subdata(in: 0 ..< self.count - kCCBlockSizeAES128)
         var decryptedData = Data(count: size_t(self.count - iv.count))
         var numBytesDecrypted: size_t = 0
-        
-        let cryptStatus = decryptedData.withUnsafeMutableBytes { cryptBytes in
-            dataToBeDecrypted.withUnsafeBytes { dataBytes in
-                key.withUnsafeBytes { (keyBytes: UnsafePointer<UInt8>) in
-                    iv.withUnsafeBytes { (ivBytes: UnsafePointer<UInt8>) in
-                        CCCrypt(CCOperation(kCCDecrypt),
-                                CCAlgorithm(kCCAlgorithmAES),
-                                CCOptions(kCCOptionPKCS7Padding),
-                                keyBytes,
-                                kCCKeySizeAES256,
-                                ivBytes,
-                                dataBytes,
-                                self.count,
-                                cryptBytes,
-                                decryptedData.count,
-                                &numBytesDecrypted)
-                    }
-                }
-            }
+        let status = CCCrypt(CCOperation(kCCDecrypt),
+                             CCAlgorithm(kCCAlgorithmAES),
+                             CCOptions(kCCOptionPKCS7Padding),
+                             key.pointer,
+                             kCCKeySizeAES256,
+                             iv.pointer,
+                             dataToBeDecrypted.pointer,
+                             self.count,
+                             decryptedData.mutablePointer(),
+                             decryptedData.count,
+                             &numBytesDecrypted)
+        if UInt32(status) == UInt32(kCCSuccess) && numBytesDecrypted > 0 {
+            return decryptedData
+        } else {
+            throw SecureStorageError.decryptionFailed
         }
-        
-        return decryptedData
     }
     
 }
